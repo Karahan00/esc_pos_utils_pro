81cdfe916ca83f32ca6f8bccb2363e539e2dbfcc
Karahan Osman <karahan.osman@epppuls.com>
2024-06-14 22:52:04 +0200

    Renamed export

--- example/main.dart
+++ example/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_esc_pos_utils_image_3/esc_pos_utils_pro.dart';
+import 'package:esc_pos_utils_pro/esc_pos_utils_pro.dart';
 
 Future<List<int>> main() async {
   final profile = await CapabilityProfile.load();
--- test/flutter_esc_pos_utils_test.dart
+++ test/flutter_esc_pos_utils_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_esc_pos_utils_image_3/esc_pos_utils_pro.dart';
-
 void main() {
   test('adds one to input values', () {});
 }
