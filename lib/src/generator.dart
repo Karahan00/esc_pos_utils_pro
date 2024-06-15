821043696a1f4257380760631dfb7e9013c358cb
Karahan Osman <karahan.osman@epppuls.com>
2024-06-15 11:48:51 +0200

    Updated capability profile for char encoding sets on esc pos epson printers and fixed generator for european text encodings

--- lib/src/generator.dart
+++ lib/src/generator.dart
@@ -67,12 +67,7 @@ class Generator {
         .replaceAll(" ", ' ')
         .replaceAll("•", '.');
     if (!isKanji) {
-      if(text.contains("€")) {
-        return ascii.encode(text);
-      }
-      else {
-        return codec.encode(text);
-      }
+      return codec.encode(text);
     } else {
       return Uint8List.fromList(gbk_bytes.encode(text));
     }
